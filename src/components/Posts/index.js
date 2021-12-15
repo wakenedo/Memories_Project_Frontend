310ef058bcf695d88766feb1789d3f43d0eff615
wakenedo <a_nuner@outlook.com>
2021-12-15 11:57:52 -0300

    Frontend - Memories Project - Part 2
    Added:
    @materialui/icons
    
    Background:
    Added a pre-made BG.
    Quick and easy BGs with "https://www.svgbackgrounds.com/"

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Grid, CircularProgress } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 
 import useStyles from './styles'
@@ -10,11 +11,15 @@ const Posts = () => {
 
     console.log(posts)
     return (
-        <>
-            <h1>POSTS COMPONENT</h1>
-            <Post />
-            <Post />
-        </>
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={classes.container} container alignItems='stretch' spacing={3}> 
+                {posts.map((post) => (
+                    <Grid key={post._id} item xs={12} sm={6}>
+                        <Post post={post} />
+                    </Grid>
+                ))}
+            </Grid>
+        )
     )
 }
 
